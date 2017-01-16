d983bc70f8a35a369dae7bed5614319b209c50db
cocoaroger <cocoaroger@163.com>
2017-01-16 10:46:13 +0800

    微调

---
+++ Algorithm/Algorithm/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Algorithm
+//
+//  Created by roger wu on 2017/1/16.
+//  Copyright © 2017年 cocoaroger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Algorithm
 数据结构与算法分析
+---
+### <a href="Linear List.md">线性表的顺序存储和链式存储<a/>
+
