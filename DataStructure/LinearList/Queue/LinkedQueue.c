853e679154ac03ba553f6c45a24f93bc8f628968
cocoaroger <cocoaroger@163.com>
2017-02-07 09:25:14 +0800

    解决bug

--- DataStructure/LinearList/Queue/LinkedQueue.c
+++ DataStructure/LinearList/Queue/LinkedQueue.c
@@ -8,6 +8,7 @@
 typedef int ElementType;
 typedef struct _Node Node;
 typedef struct _LinkedQueue LinkedQueue;
+#define NODE_SIZE sizeof(struct _Node)
 
 struct _Node
 {
@@ -36,14 +37,19 @@ void printQueue(LinkedQueue *queue)
 
 LinkedQueue* init()
 {
-	LinkedQueue *queue = (LinkedQueue*)malloc(sizeof(LinkedQueue*));
+	LinkedQueue *queue = (LinkedQueue*)malloc(sizeof(LinkedQueue));
+	
+	// head,tail必须初始化为NULL，否则会出问题,可能是嘢指针导致
+	queue->head = NULL;
+	queue->tail = NULL;
+	
 	queue->size = 0;
 	return queue;
 }
 
 void inQueue(LinkedQueue *queue, ElementType data)
 {
-	Node *newNode = (Node*)malloc(sizeof(Node*));
+	Node *newNode = (Node*)malloc(NODE_SIZE);
 	newNode->data = data;
 	newNode->next = NULL;
 	
@@ -83,22 +89,20 @@ int main(int argc, char const *argv[])
 	LinkedQueue *queue = init();
 	int count = 10;
 	printf("入队列的数据：\n");
-	for (int i = 0; i < count; ++i)
+	for (int i = 0; i < count; i++)
 	{
-		ElementType inData = (i);
+		ElementType inData = (i+100);
 		inQueue(queue, inData);
 		printf("%d\t", inData);
 	}
 	printf("\n-----入队列结束-----\n");
 	printQueue(queue);
 
-	for (int i = 0; i < count; ++i)
+	for (int i = 0; i < count; i++)
 	{
 		ElementType outData = outQueue(queue);
 		printf("%d\t", outData);
 	}
 	printf("\n-----出队列结束-----\n");
-
-
 	return 0;
 }
