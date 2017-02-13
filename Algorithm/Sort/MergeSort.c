edb5efacce86bd157f06590a4e35628b48fe979a
cocoaroger <cocoaroger@163.com>
2017-02-13 23:55:58 +0800

    完成归并算法

--- Algorithm/Sort/MergeSort.c
+++ Algorithm/Sort/MergeSort.c
@@ -1,5 +1,8 @@
 /**
  * 归并排序
+ * 时间复杂度为O(logn * n)
+ * 空间复杂度O(n)
+ * 排序效率是相对较高的
  */
 
 /**
@@ -10,31 +13,67 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-// 一个数组左右半边分别有序，归并
-void merge(int array[], int down, int mid, int up)
+// 将有二个有序数列a[first...mid]和a[mid...last]合并。  
+void merge(int array[], int first, int mid, int last, int *temp)
 {
-	int i = down, j = mid + 1;
-
-	// 复制数组中元素
-	for (int k = down; k <= up; k++)
+	int i = first, j = mid + 1; // 两个数组的起始位置
+	int m = mid, n = last; // 结束位置
+	int k = 0;
+	
+	while(i <= m && j <= n) 
 	{
-		
+		if (array[i] <= array[j]) {
+			temp[k++] = array[i++];
+		} else {
+			temp[k++] = array[j++];
+		}
+	}
+
+	while(i <= m)
+	{
+		temp[k++] = array[i++];
+	}
+	while(j <= n) 
+	{
+		temp[k++] = array[j++];
+	}
+
+	for (i = 0; i < k; i++)
+	{
+		array[first+i] = temp[i];
 	}
 }
 
-// 核心算法
-void mergeSort(int array[], int down, int up)
+// 核心算法，递归分解数列
+void mergeSort(int array[], int first, int last, int *temp)
 {
-	if (up <= down) return; // 结束条件
-	int mid = (up - down)/2 + down;
-	mergeSort(array, down, mid); // 左半边排序
-	mergeSort(array, mid+1, up); // 右半边排序
-	merge(array, down, mid, up);
+	if (first < last) {
+		int mid = (first+last)/2;
+		mergeSort(array, first, mid, temp); // 左半边排序
+		mergeSort(array, mid+1, last, temp); // 右半边排序
+		merge(array, first, mid, last, temp);
+	}
 }
 
-void sort(int array[], int length)
+int* sort(int array[], int length)
 {
-	mergeSort(array, 0, length - 1);
+	int *temp = (int*)malloc(sizeof(int) * length);
+	mergeSort(array, 0, length - 1, temp);
+	return temp;
+}
+
+int main(int argc, char const *argv[])
+{
+	int data[5] = {20, 5, 9, 1, 3};
+	int *temp = sort(data, 5);
+
+	for (int i = 0; i < 5; ++i)
+	{
+		printf("%d\n", temp[i]);
+	}
+
+	return 0;
 }
 
