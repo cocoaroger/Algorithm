df3ac44c6bcb590fb7788252e92eafb41ef6778c
cocoaroger <cocoaroger@163.com>
2017-02-15 22:00:33 +0800

    实现快排 迭代法

---
+++ Algorithm/Sort/QuickSort.c
@@ -0,0 +1,88 @@
+/**
+ * 快速排序
+ *
+ * 排序思想：通过一趟排序将要排序的数据切分成独立的两部分，
+ * 其中一部分的所有数据都比另外一部分的所有数据都要小，
+ * 然后再按此方法对这两部分数据分别进行快速排序，
+ * 整个排序过程可以递归进行，以此达到整个数据变成有序序列。
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * 迭代法
+ */
+typedef struct _Range {
+	int start, end;
+} Range;
+
+Range newRange(int start, int end)
+{
+	Range r;
+	r.start = start;
+	r.end = end;
+	return r;
+}
+
+void swap(int *x, int *y)
+{
+	int t = *x;
+	*x = *y;
+	*y = t;
+}
+
+void quickSort(int array[], const int length)
+{
+	if (length <= 0)
+	{
+		return;
+	}
+	// r[]模拟堆叠，p为数量，r[p++]为push，r[--p]为pop且取得值
+	Range r[length];
+	int p = 0;
+	r[p++] = newRange(0, length-1);
+
+	while (p) {
+		Range range = r[--p];
+		if (range.start >= range.end)
+		{
+			continue;
+		}
+
+		int mid = array[range.end];
+		int left = range.start, right = range.end - 1;
+		while (left < right) {
+			while (array[left] < mid && left < right) {
+				left ++;
+			}
+			while (array[right] >= mid && left < right) {
+				right --;
+			}
+			swap(&array[left], &array[right]);
+		}
+
+		if (array[left] >= array[range.end]) {
+			swap(&array[left], &array[range.end]);
+		} else {
+			left++;
+		}
+		r[p++] = newRange(range.start, left-1);
+		r[p++] = newRange(left+1, range.end);
+	}
+}
+
+
+int main(int argc, char const *argv[])
+{
+	int array[] = {10, 2, 3, 1, 5, 30, 12, 18, 25, 44, 45};
+	int length = sizeof(array)/sizeof(array[0]);
+	quickSort(array, length);
+	for (int i = 0; i < length; ++i)
+	{
+		printf("%d\n", array[i]);
+	}
+	return 0;
+}
--- Algorithm/Sort/ShellSort.c
+++ Algorithm/Sort/ShellSort.c
@@ -2,6 +2,9 @@
  * 希尔排序
  * 也称递减增量排序算法，是插入排序的一种更高效的改进版本。希尔排序是非稳定排序算法。
  * https://zh.wikipedia.org/wiki/%E5%B8%8C%E5%B0%94%E6%8E%92%E5%BA%8F
+ *
+ * 时间复杂度：根据步长序列的不同而不同。已知最好的： O(nlog2n)
+ * 空间复杂度	：O(n)
  */
 
 #include <stdio.h>
@@ -11,7 +14,7 @@ void shellSort(int array[], int length)
 {
 	int i, j, gap;
 	int temp;
-	for (gap = length >> 1; gap > 0; gap >>= 1) // 步长
+	for (gap = length >> 1; gap > 0; gap >>= 1) // 步长,每次右移一位，除以2
 	{
 		for (i = gap; i < length; i++)
 		{
