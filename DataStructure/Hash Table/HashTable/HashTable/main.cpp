c19cf90e0b0b524b16e9cf939360b231ba68a673
cocoaroger <cocoaroger@163.com>
2017-03-16 23:11:07 +0800

    Hash需要解决的问题

--- DataStructure/Hash Table/HashTable/HashTable/main.cpp
+++ DataStructure/Hash Table/HashTable/HashTable/main.cpp
@@ -6,6 +6,14 @@
 //  Copyright © 2017年 cocoaroger. All rights reserved.
 //
 
+// 需要解决的问题
+//   1）能不能支持所有的对象而不仅限于整数？
+//　　2）如何支持所有整数而不只是正整数？
+//　　3）被删除了的槽仍然占用查找时间。
+//　　4）随着时间的推移，被标记为碰撞的槽越来越多，怎么办？
+//　　5）必须在创建容器的时候指定大小，不能自动扩张。
+//　　6）只是一个 HashSet，而不是HashTable。
+
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
