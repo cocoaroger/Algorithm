ce57f2514c0d0e703670f9c113a310c531ca5846
cocoaroger <cocoaroger@163.com>
2017-03-24 23:00:29 +0800

    学习 Kruskal 计算加权连通图的最小生成树

---
+++ Algorithm/Kruskal/Kruskal/Kruskal/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Kruskal
+//
+//  Created by roger wu on 2017/3/24.
+//  Copyright © 2017年 cocoaroger. All rights reserved.
+//
+
+// kruskal算法，计算加权连通图的最小生成树的算法
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
